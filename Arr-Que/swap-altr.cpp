1d74b79f00d04ffafed8bb8b95dda98acb1f15e2
MamoonShaikh <mamoonshaikh5598@gmail.com>
2024-06-01 18:33:35 +0530

    swap alternate numbers of array

--- Arr-Que/swap-altr.cpp
+++ Arr-Que/swap-altr.cpp
@@ -1,30 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void print(int arr[], int n){
-    for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
+// Function to print alternate array
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
         // cout<<endl;
     }
 }
-void swapalternate(int arr[],int size ){
-    for(int i=0; i<size;i+=2){
-        if(i+1<size){
-            swap(arr[i], arr[i+1]);
+// Function to swap alternate array
+void swapalternate(int arr[], int size)
+{
+    for (int i = 0; i < size; i += 2)
+    {
+        if (i + 1 < size)
+        {
+            swap(arr[i], arr[i + 1]);
         }
     }
 }
 
-int main(){
-    int even[8]={2,3,5,4,6,8,11,10};
-    int odd[9]={1,2,3,4,5,6,7,8,9};
+int main()
+{
+    int even[8] = {2, 3, 5, 4, 6, 8, 11, 10};//even array
+    int odd[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};//odd array
 
-    swapalternate(even,8);
-    print(even,8);
-    cout<<endl;
-    swapalternate(odd,9);
-    print(odd,9);
-   
 
-    
+// swap and print even array
+    swapalternate(even, 8);
+    print(even, 8);
+    cout << endl;//change new line
+    //print and swap odd array
+    swapalternate(odd, 9);
+    print(odd, 9);
+
     return 0;
 }
